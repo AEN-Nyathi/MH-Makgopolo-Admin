654ddb216cf81cb9034b1105ac0e71aef240c606
Dr Chris Ntsako <drchrisntsako@gmail.com>
2025-10-11 07:51:30 +0000

    for now let us allow users to use the app without logging in, we will fi

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,4 @@
+
 import { type NextRequest, NextResponse } from 'next/server'
 import { createServerClient } from '@supabase/ssr'
 
@@ -24,17 +25,17 @@ export async function middleware(request: NextRequest) {
     }
   );
 
-  const { data: { session } } = await supabase.auth.getSession();
+  // const { data: { session } } = await supabase.auth.getSession();
 
   const { pathname } = request.nextUrl;
 
-  if (!session && pathname.startsWith('/admin') && pathname !== '/admin/login') {
-    return NextResponse.redirect(new URL('/admin/login', request.url));
-  }
+  // if (!session && pathname.startsWith('/admin') && pathname !== '/admin/login') {
+  //   return NextResponse.redirect(new URL('/admin/login', request.url));
+  // }
 
-  if (session && pathname === '/admin/login') {
-    return NextResponse.redirect(new URL('/admin', request.url));
-  }
+  // if (session && pathname === '/admin/login') {
+  //   return NextResponse.redirect(new URL('/admin', request.url));
+  // }
   
   return response;
 }
